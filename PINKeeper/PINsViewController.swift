8a8079c14f4b13a365cb6ea72a2912426431487f
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-08-09 19:25:19 +0300

    Added PINsViewController.

---
+++ PINKeeper/PINsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PINsViewController.swift
+//  PINKeeper
+//
+//  Created by Alexey Tsymlov on 8/9/15.
+//  Copyright (c) 2015 Alexey Tsymlov. All rights reserved.
+//
+
+import UIKit
+
+class PINsViewController: UIViewController {
+
+}
