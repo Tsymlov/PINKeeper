9c3a32fc9e37c97fc3085d2f3048316cd87adf07
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-08-13 14:23:22 +0300

    Added Settings model class.

---
+++ PINKeeper/Model/Settings.swift
@@ -0,0 +1,30 @@
+//
+//  Settings.swift
+//  PINKeeper
+//
+//  Created by Alexey Tsymlov on 8/13/15.
+//  Copyright (c) 2015 Alexey Tsymlov. All rights reserved.
+//
+
+import Foundation
+
+class Settings {
+    
+    struct Constants {
+        struct UserDefaultsKeys {
+            static let isTouchIDBlockedKey = "com.alexeytsymlov.PINKeeper.Settings.isTouchIDBlocked"
+        }
+    }
+    
+    private static let userDefaults = NSUserDefaults.standardUserDefaults()
+    
+    class var isTouchIDBlocked: Bool{
+        set{
+            userDefaults.setBool(newValue, forKey: Constants.UserDefaultsKeys.isTouchIDBlockedKey)
+            if !userDefaults.synchronize(){ println("UserDefaults synchronization failed!") }
+        }
+        get{
+            return userDefaults.boolForKey(Constants.UserDefaultsKeys.isTouchIDBlockedKey)
+        }
+    }
+}
