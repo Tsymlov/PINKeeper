1eded0bbd5ecd921c280aaec7bf9747a02ace001
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-08-19 16:52:34 +0300

    Added PIN class.

---
+++ PINKeeper/Model/PIN.swift
@@ -0,0 +1,14 @@
+//
+//  PIN.swift
+//  PINKeeper
+//
+//  Created by Alexey Tsymlov on 8/19/15.
+//  Copyright (c) 2015 Alexey Tsymlov. All rights reserved.
+//
+
+import Foundation
+
+class PIN {
+    var description = ""
+    var value = ""
+}
