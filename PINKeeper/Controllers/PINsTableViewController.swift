5f0561fcab41ac2c8fc83d78f5857308716ddd1d
Alexey Tsymlov <alexey.tsymlov@gmail.com>
2015-08-24 19:46:23 +0300

    Added removing of PINs.

--- PINKeeper/Controllers/PINsTableViewController.swift
+++ PINKeeper/Controllers/PINsTableViewController.swift
@@ -19,13 +19,18 @@ class PINsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.pins[0] = PIN.MR_findAll() as! [PIN]
-        println("Found \(pins[0].count) records in database")
+        refreshData()
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    private func refreshData(){
+        self.pins[0] = PIN.MR_findAll() as! [PIN]
+        println("Found \(pins[0].count) records in database")
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -50,25 +55,24 @@ class PINsTableViewController: UITableViewController {
         return cell
     }
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return NO if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            // Delete the row from the data source
+            pins[indexPath.section][indexPath.row].MR_deleteEntity()
+            refreshData()
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
